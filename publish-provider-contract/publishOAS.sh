278759915ab0b7ce003816935474885e28216bfc
Johnny <johnnydongelmans@gmail.com>
2024-05-17 11:44:48 +0200

    fix #43
    Add optional input for publish-provider-contract action
    which allows you override the OAS file content-type

--- publish-provider-contract/publishOAS.sh
+++ publish-provider-contract/publishOAS.sh
@@ -19,11 +19,13 @@ REPORT_FILE_CONTENT_TYPE=${REPORT_FILE_CONTENT_TYPE:-'text/plain'}
 VERIFIER_TOOL=${VERIFIER_TOOL:-'github-actions'}
 BRANCH=${GITHUB_REF#refs/heads/}
 BUILD_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
+oas_file_content_type=${oas_file_content_type:-'application/yaml'}
 
 echo """
 URL: $URL
 PACT_BROKER_TOKEN : $PACT_BROKER_TOKEN
 oas_file: $oas_file
+oas_file_content_type: $oas_file_content_type
 results_file: $results_file
 EXIT_CODE: $EXIT_CODE
 BRANCH: $BRANCH
@@ -41,7 +43,7 @@ docker run --rm \
   --provider $application_name \
   --provider-app-version $version \
   --branch $BRANCH \
-  --content-type application/yaml \
+  --content-type $oas_file_content_type \
   --verification-exit-code=$EXIT_CODE \
   --verification-results $results_file \
   --verification-results-content-type $REPORT_FILE_CONTENT_TYPE \
